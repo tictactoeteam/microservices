aa5ddf3d29743e1f9e425a2509b1dce9acb85de3
David Cruz <david@typokign.com>
2019-04-30 22:17:23 -0700

    Give auth-service something to keep busy

--- services/auth-service/src/main/java/edu/saddleback/microservices/auth/Main.java
+++ services/auth-service/src/main/java/edu/saddleback/microservices/auth/Main.java
@@ -1,7 +1,10 @@
 package edu.saddleback.microservices.auth;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws InterruptedException {
+        // lol k8s thinks the pod is crashing if I don't give it something to keep busy
+        while (true) {
+            Thread.sleep(5000);
+        }
     }
 }
