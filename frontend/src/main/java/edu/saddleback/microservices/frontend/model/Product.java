362d31568a6af3602bf6f84fdce56a7e0c8a1913
Kai Duty <kdcptkai31@gmail.com>
2019-05-04 13:32:15 -0700

    Adds Basic UI and navigation; Cart, CartItem, Product classes

---
+++ frontend/src/main/java/edu/saddleback/microservices/frontend/model/Product.java
@@ -0,0 +1,41 @@
+package edu.saddleback.microservices.frontend.model;
+
+/**
+ * Represents a product, with name, image, and price.
+ */
+public class Product {
+
+    private String name;
+    private String imagePath;
+    private double price;
+
+    /**
+     * Constructor
+     * @param name
+     * @param imagePath
+     * @param price
+     */
+    public Product(String name, String imagePath, double price){
+
+        this.name = name;
+        this.imagePath = imagePath;
+        this.price = price;
+
+    }
+
+    /**
+     * Default Constructor
+     */
+    public Product(){this("", "", 0.00);}
+
+    //Getters
+    public String getName(){return name;}
+    public String getImagePath(){return imagePath;}
+    public double getPrice(){return price;}
+
+    //Setters
+    public void setName(String str){name = str;}
+    public void setImagePath(String str){imagePath = str;}
+    public void setPrice(double d){price = d;}
+
+}
---
+++ frontend/src/main/java/edu/saddleback/microservices/frontend/view/CartView.java
@@ -0,0 +1,40 @@
+package edu.saddleback.microservices.frontend.view;
+
+import edu.saddleback.microservices.frontend.controller.AppController;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+
+/**
+ * Controls the cart.fxml page, including showing your cart items with a checkout option.
+ */
+public class CartView {
+
+    private AppController controller;
+
+    @FXML
+    private Label usernameLabel;
+    @FXML
+    private ListView cartList;
+
+
+    public void initialize(){
+
+        controller = App.getController();
+        usernameLabel.setText(controller.getLoggedInUsername());
+
+    }
+
+    /**
+     * Returns the user to the main product page.
+     */
+    public void onShopButtonClicked(){
+
+        try {
+            App.getCoordinator().showAppScene();
+        }catch (Exception e){}
+
+    }
+
+}
