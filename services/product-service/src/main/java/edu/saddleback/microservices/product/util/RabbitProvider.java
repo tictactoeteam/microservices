0c191d6723e064a3e1bb70cff5acc49dcfab5ea6
datowlcs <datowlcs@gmail.com>
2019-05-14 11:54:22 -0700

    Added RabbitProvider and util package

---
+++ services/product-service/src/main/java/edu/saddleback/microservices/product/util/RabbitProvider.java
@@ -0,0 +1,53 @@
+package edu.saddleback.microservices.product.util;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+public class RabbitProvider {
+    private static String USERNAME = System.getenv("RABBIT_USERNAME");
+    private static String PASSWORD = System.getenv("RABBIT_PASSWORD");
+
+    private static Connection connection;
+    private static Channel channel;
+
+    public static Connection getConnection() {
+        if (connection != null) {
+            return connection;
+        }
+
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setUsername(USERNAME);
+        factory.setPassword(PASSWORD);
+        factory.setHost("rabbitmq");
+
+        try {
+            connection = factory.newConnection();
+        } catch (IOException | TimeoutException e) {
+            e.printStackTrace();
+        }
+
+        return connection;
+    }
+
+    public static Channel getChannel() {
+        if (channel != null) {
+            return channel;
+        }
+
+        try {
+            channel = getConnection().createChannel();
+
+            channel.exchangeDeclare("user", "direct", true);
+            channel.queueDeclare("user-created", true, false, false, null);
+            channel.queueBind("user-created", "user", "created");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return channel;
+    }
+}
