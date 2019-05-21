b0fb53c1207fd40a3c4d816a7ab6c5733003f7a3
Tomasz <gruszkat98@gmail.com>
2019-05-20 17:03:29 -0700

    fix_lint

--- services/order-service/src/main/java/edu/saddleback/microservices/order/controller/RabbitController.java
+++ services/order-service/src/main/java/edu/saddleback/microservices/order/controller/RabbitController.java
@@ -1,11 +1,11 @@
 package edu.saddleback.microservices.order.controller;
 
-import com.rabbitmq.client.Connection;
-import com.rabbitmq.client.Channel;
-import com.rabbitmq.client.ConnectionFactory;
-
 import java.io.IOException;
 import java.util.concurrent.TimeoutException;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
 
 public class RabbitController {
     private static String USERNAME = System.getenv("RABBIT_USERNAME");
