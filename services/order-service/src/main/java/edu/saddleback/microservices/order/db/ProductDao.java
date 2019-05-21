af2966283db5031f975f0a999cf46f16d75f5d41
David Cruz <david@typokign.com>
2019-05-21 00:07:00 -0700

    fix uuid

--- services/order-service/src/main/java/edu/saddleback/microservices/order/db/ProductDao.java
+++ services/order-service/src/main/java/edu/saddleback/microservices/order/db/ProductDao.java
@@ -13,14 +13,10 @@ public class ProductDao  {
         Connection connection = DbManager.getConnection();
 
         PreparedStatement statement = connection.prepareStatement("SELECT SUM(price) FROM product " +
-                "WHERE id IN ?");
+                "WHERE id IN ?::uuid[]");
 
         String[] ids = cart.stream().map((cartObject) -> cartObject.product).toArray(String[]::new);
-        for (String id : ids) {
-            System.out.println(id);
-        }
-        System.out.println(connection.createArrayOf("uuid", ids));
-        statement.setArray(1, connection.createArrayOf("uuid", ids));
+        statement.setArray(1, connection.createArrayOf("text", ids));
 
         ResultSet rs = statement.executeQuery();
 
